8cdc8324861f11451e0e50d01c30da36c429e674
Ivan Marban <ivan.marban@gmail.com>
2018-04-08 12:02:17 +0200

    Include jQuery in webpack renderer config

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -123,7 +123,11 @@ let rendererConfig = {
         : false
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
   ],
   output: {
     filename: '[name].js',
