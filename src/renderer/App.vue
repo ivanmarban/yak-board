e754b357882c1e972fe17655749d704300c82683
Ivan Marban <ivan.marban@gmail.com>
2018-04-14 13:25:28 +0200

    Main app vue file

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="wrapper">
+    <task-board></task-board>
+  </div>
+</template>
+
+<script>
+  import TaskBoard from '@/components/TaskBoard'
+
+  export default {
+    components: {
+      TaskBoard
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
