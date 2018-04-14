91515e32e4cb1802010e2f4b68ee34275fbb0b82
Ivan Marban <ivan.marban@gmail.com>
2018-04-14 13:32:36 +0200

    Include imports in main script

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-
 import App from './App'
+import db from './datastore'
+import 'semantic-ui-css/semantic.css'
+import 'semantic-ui-css/semantic.js'
+window.jQuery = window.$ = require('jquery/dist/jquery.min')
 
 Vue.config.productionTip = false
+Vue.prototype.$db = db
 
 /* eslint-disable no-new */
 new Vue({
